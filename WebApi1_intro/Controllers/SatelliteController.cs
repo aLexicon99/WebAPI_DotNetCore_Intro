5b64718d08e2c1b90cb9365cb6629c84c4b3db65
aLexicon99 <267594691+aLexicon99@users.noreply.github.com>
2026-05-26 11:39:48 +0200

    added-satellite-controller
    new route /satellite
    -New Controller
    -New Model

---
+++ WebApi1_intro/Controllers/SatelliteController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi1_intro.Models;
+
+namespace WebApi1_intro.Controllers
+{
+    [ApiController]
+    [Route("[controller]")] // https://localhost:7266/satellite
+    public class SatelliteController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Index()
+        {
+            return "This is the response from the Satellite Index 'action'";
+        }
+
+        [HttpGet]
+        [Route("satdetails")]
+        public ActionResult<Satellite> Details(int Id)
+        {
+            Satellite S = new Satellite()
+            {
+                Id = Id,
+                Model = "XYZ123",
+                MaxHeight = 200
+            };
+            return S;
+        }
+    }
+}
---
+++ WebApi1_intro/Models/Satellite.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi1_intro.Models
+{
+    public class Satellite
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+
+        public int MaxHeight { get; set; }
+        
+    }
+}
